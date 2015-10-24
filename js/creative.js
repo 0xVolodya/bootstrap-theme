d0e51042e8ba368e43a309a25886e8ef26a221c4
Vladimir Lebedev <lebedev.vladimir3@gmail.com>
2015-10-24 11:58:45 +0300

    Что использовал в проекте: js, jquery, bootstrap, animate,font-awesome. Время: 2 дня

--- js/creative.js
+++ js/creative.js
@@ -3,11 +3,30 @@
  */
 
 (function ($) {
+    //при нажатии панели происходит перемещение страницы в указанную область
     "use strict";
+    $('.page-scroll').bind('click', function (event) {
+        event.preventDefault();
+        var $pagescroll=$(this);
+        $('html, body').stop().animate({
+
+            scrollTop:($($pagescroll.attr('href')).offset().top -50)
+        },1500,"easeInOutExpo");
+    });
+
+    //при прокрутке выделяются элементы навигации
     $('body').scrollspy({
         target:'.navbar-fixed-top',
-        offset:100
+        offset:55
     });
+
+    //смещение главное панели навигации
+    $("#mainNav").affix({
+        offset:{
+            top:100
+        }
+    });
+
     //alert("blablba");
     new WOW().init();
 })(jQuery);
